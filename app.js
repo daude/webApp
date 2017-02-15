507d2fcc2c79f33d01cbe76f477e1ae44a0c76f4
daude <davidgomja@gmail.com>
2017-02-15 17:00:08 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Quiz from './quiz';
-import njax from 'najax';
-import $ from 'jquery';
+import $ from 'najax';
 
-njax.get('http://localhost:4444/quiz/', function( data ) {
+$.get('http://localhost:4444/quiz/', function( data ) {
 	console.log('data', JSON.parse(data));
 
 	ReactDOM.render(
