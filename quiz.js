61ddea952296ae1b33ec6ab2aeacc7cc14838e72
daude <davidgomja@gmail.com>
2017-02-17 15:59:15 +0545

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import _ from 'lodash';
 import Answer from './Answer';
+import result from './result';
 
 class OneQuestion extends React.Component {
 	render() {
-		//console.log(this)
-		//console.log('loading question:', this.props.quiz.id)
 		return (
 			<form onSubmit={this.props.handleSubmit} key={Math.random()}>
 				<div>
-					<h3 className="text-info" ref='nam'>{this.props.quiz.name}</h3>
+					<h3 className="text-info" ref='nam'>Hello {this.props.quiz.name}</h3>
 					<p className="text-success" ref='ques'>{this.props.quiz.question}</p>
 					<Answer answers={this.props.quiz.answer}
 						ref='ans'
-						questionId={this.props.quiz._id}
+						questionId={this.props.id}
 						handleClick={this.props.handleClick}
-						//value={this.state.value}
-						//onClick={this.props.handleClick}
 					/>
 				</div>
 			</form>
@@ -33,36 +30,24 @@ class Quiz extends React.Component {
 		this.state = {
 			value: '',
 			currentQuestion: currentQuestion,
-			button: false,
 			answers: {},
 		};
 	}
 
 	showPrevQuestion() {
-		if (this.state.currentQuestion < 1) {
-			return
-		}
 		this.setState({
 			currentQuestion: this.state.currentQuestion - 1,
-			button: false,
 		})
+
 	}
 
 	showNextQuestion() {
-		if (this.state.currentQuestion > 9) {
-			return
-		}
 		this.setState({
 			currentQuestion: this.state.currentQuestion + 1,
-			button: false,
 		})
 	}
 
 	handleSaveAnswer(id, value) {
-		//event.preventDefault();
-
-		console.log('question id:', id)
-		console.log('value', value)
 
 		let newAnswers = this.state.answers
 
@@ -70,48 +55,59 @@ class Quiz extends React.Component {
 
 		this.setState({
 			answers: newAnswers,
-			button: true,
 			value: value
 		})
-		console.log('my answer list:', this.state.answers)
-
 	}
 
 	render() {
-		//console.log('State:', this.state)
+		/*console.log('State:', this.state)
 
-		/*console.log('number:', this.state.currentQuestion)
-		console.log('all questions:', this.props.allQuestions.length)
-*/
+		console.log('number:', this.state.currentQuestion)
+		console.log('all questions:', this.props.allQuestions.length)*/
+
 		let question = this.props.allQuestions[this.state.currentQuestion];
 
-		console.log(_.isUndefined(question))
+		//console.log('Checking question:', _.isUndefined(question))
+		//console.log(_.isUndefined(question))
+		/*console.log('this.state.answers', this.state.answers)
+		console.log('this.state.currentQuestion', this.state.currentQuestion)*/
+
 
 		return (
 			<div>
 				{_.isUndefined(question) ? <d/> :
 					<div className='demo'>
 						<OneQuestion
+							id={this.state.currentQuestion}
 							quiz={question}
 							handleClick={this.handleSaveAnswer.bind(this)}
-							value={this.state.value}/>
+							value={this.state.value}
+							//onClick={this.showPrevQuestion.bind(this)}
+							/>
 						<div>
-							{this.state.button === true ?
+							{this.state.currentQuestion > 0 ?
 								<button type='button'
-								//value={this.state.value}
-								onClick={this.showPrevQuestion.bind(this)}
-								className='btn btn-success'>Prev</button> : null
+									onClick={this.showPrevQuestion.bind(this)}
+									className='btn btn-success'
+								>
+									Prev
+								</button> : null
+
 							}
-							{this.state.button === true?
-							<button type='button'onClick={this.showNextQuestion.bind(this)}
-							className='btn btn-success' >Next</button> : null
+							{this.state.currentQuestion >= 0 && this.state.answers[this.state.currentQuestion] ?
+								<button type='button'
+									onClick={this.showNextQuestion.bind(this)}
+									className='btn btn-success'
+								>
+									Next
+								</button> : null
 							}
 						</div>
 					</div>
 				}
 				<div>
-					{this.state.value ?
-						'your answer is ' + this.state.value :
+					{this.state.answers[this.state.currentQuestion] ?
+						'your answer is ' + this.state.value:
 						'You have not chosen an answer yet'
 					}
 				</div>
