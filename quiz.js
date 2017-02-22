b6129aba1abc20b2548e4ab2d6789bb75c090b5f
daude <davidgomja@gmail.com>
2017-02-22 16:09:51 +0545

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import _ from 'lodash';
 import Answer from './Answer';
-import result from './result';
+import Result from './result';
+import $ from 'jquery';
 
 class OneQuestion extends React.Component {
 	render() {
+		//console.log('questionId------>', this.props.id)
 		return (
 			<form onSubmit={this.props.handleSubmit} key={Math.random()}>
 				<div>
@@ -59,60 +61,96 @@ class Quiz extends React.Component {
 		})
 	}
 
+	saveData() {
+
+		$.ajax({
+			type: 'POST',
+			url: 'http://localhost:4444/answers/',
+			data: {
+
+			},
+			success: function( data ) {
+				console.log('data', JSON.parse(data));
+			},
+			// dataType: dataType
+		});
+	}
+
 	render() {
+		//console.log('questionId', this.state.questionId);
+		//console.log('value', this.state.value);
 		/*console.log('State:', this.state)
 
 		console.log('number:', this.state.currentQuestion)
 		console.log('all questions:', this.props.allQuestions.length)*/
 
 		let question = this.props.allQuestions[this.state.currentQuestion];
-
+		//console.log(this.props)
 		//console.log('Checking question:', _.isUndefined(question))
 		//console.log(_.isUndefined(question))
-		/*console.log('this.state.answers', this.state.answers)
-		console.log('this.state.currentQuestion', this.state.currentQuestion)*/
+		//console.log('this.state.answers', this.props.allQuestions)
+		//console.log('this.state.answers', this.state.answers)
 
-
-		return (
-			<div>
-				{_.isUndefined(question) ? <d/> :
-					<div className='demo'>
-						<OneQuestion
-							id={this.state.currentQuestion}
-							quiz={question}
-							handleClick={this.handleSaveAnswer.bind(this)}
-							value={this.state.value}
-							//onClick={this.showPrevQuestion.bind(this)}
-							/>
-						<div>
-							{this.state.currentQuestion > 0 ?
-								<button type='button'
-									onClick={this.showPrevQuestion.bind(this)}
-									className='btn btn-success'
-								>
-									Prev
-								</button> : null
-
-							}
-							{this.state.currentQuestion >= 0 && this.state.answers[this.state.currentQuestion] ?
-								<button type='button'
-									onClick={this.showNextQuestion.bind(this)}
-									className='btn btn-success'
-								>
-									Next
-								</button> : null
-							}
-						</div>
-					</div>
-				}
+		if (this.props.allQuestions.length > this.state.currentQuestion) {
+			return (
 				<div>
-					{this.state.answers[this.state.currentQuestion] ?
-						'your answer is ' + this.state.value:
-						'You have not chosen an answer yet'
+					{_.isUndefined(question) ? <d/> :
+						<div className='demo'>
+							<OneQuestion
+								id={question._id}
+								// id={this.state.currentQuestiondddd}
+								quiz={question}
+								handleClick={this.handleSaveAnswer.bind(this)}
+								value={this.state.value}
+								//result={this.props.allQuestions}
+								//onClick={this.showPrevQuestion.bind(this)}
+								/>
+							<div>
+								{this.state.currentQuestion > 0 ?
+									<button type='button'
+										onClick={this.showPrevQuestion.bind(this)}
+										className='btn btn-success'
+									>
+										Prev
+									</button> : null
+
+								}
+								{this.state.currentQuestion >= 0 && this.state.answers[question._id] ?
+									<button type='button'
+										onClick={this.showNextQuestion.bind(this)}
+										className='btn btn-success'
+									>
+										Next
+									</button> : null
+								}
+							</div>
+						</div>
 					}
+					<div>
+						{this.state.answers[question._id] ?
+							'your answer is ' + this.state.value:
+							'You have not chosen an answer yet'
+						}
+					</div>
 				</div>
-			</div>
-		)
+			);
+		} else {
+			//console.log('this.state.answers', this.state.answers)
+			return (
+				<div className='btn btn-success'>
+					<p className='btn btn-info'>Quiz finished,your result are here</p>
+					<Result allQuestions={this.props.allQuestions}
+					answers={this.state.answers}
+					/>
+					<div>
+						<button type='button' className='btn btn-info'
+							onClick={this.saveData.bind(this)}>
+							saveData
+						</button>
+					</div>
+				</div>
+			);
+		}
 	}
 }
 
