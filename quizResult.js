20c3bc52895cf88e4317ce3c34391f51449107d8
David <davidgomja@gmail.com>
2017-03-02 16:40:23 +0545

    Update quizResult.js

--- quizResult.js
+++ quizResult.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import _ from 'lodash';
-//import $ from 'najax';
+import $ from 'jquery';
 
 class QuizResult extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+        }
+    }
+
+    componentDidMount() {
+       $.ajax({
+            type: 'GET',
+            url: 'http://localhost:4444/answers/',
+            //data: JSON,
+            success: function( data ) {
+                //console.log('data--->', data)
+                //console.log('data', JSON.stringify(data));
+                this.setState({data: data});
+                //console.log('this.state.data---->', this.state.data)
+            }.bind(this),
+            // dataType: dataType
+        });
+    }
 	render() {
 		return (
-			<div id='test_questions'>
-				{
-                    _.map(this.props.allQuestions, (question) => {
+			<div id='test_select'>
+                {
+                    _.map(this.state.data, (result) => {
+                        //console.log('this.props.questionId---->', this.props.questionId)
+                        console.log('this.state.data---->', result)
                         return (
                             <div key={Math.random()}>
-                                Question:{question.question}
-                                <div>
-                                    Answer:{this.props.answers[question._id]}
-                                </div>
+                                {this.result}
                             </div>
                         );
                     })
@@ -22,6 +42,6 @@ class QuizResult extends React.Component {
 		);
 	}
 }
-console.log('QuizResult---------->', QuizResult)
+//console.log('QuizResult---------->', QuizResult)
 
 export default QuizResult;
