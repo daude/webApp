ef705dfef485f477b2966d0e9efa085c6e1adf1b
David <davidgomja@gmail.com>
2017-03-02 16:34:34 +0545

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -1,25 +1,50 @@
+
 import React from 'react';
-import Quiz from './quiz';
 //import Home from './welcome';
+import Quiz from './quiz';
 import QuizResult from './quizResult';
 import Main from './main';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+//import Match from 'react-router';
+//import _ from 'lodash';
 
 //console.log('Router', Router)
 class Routes extends React.Component {
 	render() {
+		//console.log('_dirname--->', '_dirname')
 		return (
-			<Router>
-				<div>
-					<Route exact path='/' component={Main}/>
-					<Route path='/quiz' component={Quiz}/>
-					<Route path='/quizResult' component={QuizResult}/>
-				</div>
-			</Router>
+			<div>
+				<Router>
+					<div>
+						{/*<h3>What do you want to do?</h3>
+						<button className='btn btn-success'>
+							<Link to='/quiz'> PlayQuiz </Link>
+						</button>
+						<button className='btn btn-success'>
+							<Link to='/result'> DisplayResult </Link>
+						</button>
+
+						<hr/>*/}
+						<Route exact path='/' component={Main} />
+						<Route path='/quiz' component={Quiz} />
+						<Route path='/quizResult' component={QuizResult} />
+					</div>
+				</Router>
+			</div>
 		)
 	}
 }
-
+/*
+const Routes = () => (
+	<Router>
+		<div>
+			<Route exact path='/' component={Main} />
+			<Route path='/quiz' component={Quiz} />
+			<Route path='/result' component={Result} />
+		</div>
+	</Router>
+)
+*/
 export default Routes;
 
 /*const Routes = () => (
@@ -40,5 +65,5 @@ export default Routes;
 	</Router>
 )
 
-export default Routes;
-*/
+export default Routes;*/
+
