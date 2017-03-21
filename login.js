25ee7a858b5055a09a6ff7f8dc2c32758cf0deb1
David <davidgomja@gmail.com>
2017-03-21 16:04:09 +0545

    Update login.js

--- login.js
+++ login.js
@@ -11,40 +11,63 @@ class Login extends React.Component {
 
 		this.state = {
 			users: {},
+			errorusername: '',
+			errorpassword: '',
 		};
 	}
 
 	handleLogin(e) {
-		e.preventDefault()
+		e.preventDefault();
 
 		// get user name from input
 		let name = this.state.username //#1
 		//console.log('name', this.state.username)
 		if (!name) {
+			this.setState({
+				errorusername: '*username is required'
+			})
 			console.error('username is required')
-			return
+			//console.log('username is required')
 		}
 
 		let password = this.state.password // #2
 		//console.log('Password', this.state.password)
 		if (!password) {
+			this.setState({
+				errorpassword: '*password is required'
+			})
 			console.error('password is required')
-			return
+			//console.log('password is required')
 		}
 
 		// compare passwords from form and from mongo
 		let notFoundUser = (error) => {
 			console.error('error:', error)
 		}
+		/*if (!notFoundUser) {
+			console.log('user not found')
+		}*/
 
 		let foundUser = (userFromMongo) => {
+			if (!userFromMongo) {
+				//console.error('User not found')
+				this.setState({
+					invalidUser: 'Invalid username and password'
+				})
+				return
+			}
+
 			console.log('user from mongo:', userFromMongo) // #3
 			// console.log('user from mongo:', this.state.users[this.state.name]) // #3
 
 			if (userFromMongo.password === password) {
-				console.log('ok to login now')
+				//console.log('ok to login now')
 				this.setState({loginComplete: true})
 			} else {   // #4
+				/*this.setState({
+					invalidPassword: 'Password must be match'
+				})
+				return*/
 				console.log('Password must be match')
 			}
 		}
@@ -82,12 +105,17 @@ class Login extends React.Component {
 
 	updateName(event) {
 		this.setState({
+			errorusername: '',
 			username: event.target.value
-		})
+		});
+		/*if (!name) {
+			return ('username is required');
+		}*/
 	}
 
 	updatePassword(event) {
 		this.setState({
+			errorpassword: '',
 			password: event.target.value
 		})
 	}
@@ -103,19 +131,22 @@ class Login extends React.Component {
 					<Redirect from='/login' to='/main' />
 				</div>
 			)
-		} /*else {
+		}/* else {
 			<div>
-				<p>username or Password doesn't match</p>
 				<Redirect to='/main'/>
+				<p>username or Password doesn't match</p>
 			</div>
 		}*/
 		return (
 			<div className = "container">
 				<div className="wrapper">
-					<form onSubmit={this.handleSubmit}
+					<form onSubmit={this.handleLogin}
 						name="Login_Form" className="form-signin">
 						<h3 className="form-signin-heading">Welcome Back! Please Sign In</h3>
 						<hr className="colorgraph" /><br/>
+						<p className="text-danger">
+							{this.state.invalidUser}
+						</p>
 						<div>
 							<input
 								type="text"
@@ -126,6 +157,9 @@ class Login extends React.Component {
 								autoFocus=""
 								id='Username'
 							/>
+							<p className="text-danger">
+								{this.state.errorusername}
+							</p>
 						</div>
 						<div>
 							<input
@@ -137,6 +171,10 @@ class Login extends React.Component {
 								autoFocus=""
 								id='password'
 							/>
+							<p className="text-danger">
+								{this.state.errorpassword}
+{/*								{this.state.invalidUser}
+*/}							</p>
 						</div>
 						<button
 							className="btn btn-lg btn-secondary btn-block"
@@ -144,6 +182,14 @@ class Login extends React.Component {
 						>
 							Login
 						</button>
+						{
+							/*!this.state.loginComplete ? (
+							) : (
+								<p className="text-danger">
+									{this.state.erroruser}
+								</p>
+							)*/
+						}
 
 						<br/>
 						<span className="pull-right">
@@ -155,6 +201,13 @@ class Login extends React.Component {
 				</div>
 			</div>
 		);
+	/*} else {
+		return (
+			<div>
+				<p>invalid username and Password</p>
+			</div>
+		);
+	}*/
 	}
 }
 
